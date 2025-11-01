16b420c4575c639dcb143313ad3f3d07261ae74b
Алекс Коробейников <korobehka.1980@gmail.com>
2025-11-01 13:18:38 +0300

    Закрытие запущенных обходов

---
+++ RKN_Fix/KillProcesses.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RKN_Fix
+{
+    partial class KillProcesses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 12);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(471, 26);
+            progressBar1.TabIndex = 0;
+            // 
+            // KillProcesses
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(495, 50);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "KillProcesses";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "KillProcesses";
+            Load += KillProcesses_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+    }
+}
---
+++ RKN_Fix/KillProcesses.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RKN_Fix
+{
+    public partial class KillProcesses : Form
+    {
+        internal int mode = 0;
+        internal List<string> processesName = new List<string>();
+        public KillProcesses()
+        {
+            InitializeComponent();
+        }
+
+        private void KillProcesses_Load(object sender, EventArgs e)
+        {
+            switch (mode)
+            {
+                case 0:
+                    {
+                        killZapret();
+                        progressBar1.Value = 10;
+                        killOtherVPN(90);
+                        break;
+                    }
+                case 2:
+                    {
+                        killZapret();
+                        break;
+                    }
+                case 1:
+                    {
+                        killOtherVPN(100);
+                        break;
+                    }
+                default:
+                    {
+                        
+                        break;
+                    }
+            }
+            progressBar1.Value = 100;
+            this.Close();
+
+        }
+
+        private void killZapret()
+        {
+            Process[] processes = Process.GetProcessesByName("winws");
+            foreach (Process process in processes)
+            {
+                process.Kill();
+                process.Dispose();
+            }
+        }
+
+        private void killOtherVPN(int ostBar)
+        {
+            foreach (var name in processesName)
+            {
+                Process[] processes = Process.GetProcessesByName(name);
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                    process.Dispose();
+                }
+            }
+        }
+    }
+}
